3fbb545f67bccd166a9c7270b38d895ed6da6e2f
bangmookim <tugjbmyhn@naver.com>
2023-02-17 23:33:24 +0900

    전반적인 UI개선 및 상세페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,122 +1,128 @@
 
 import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
-import { useState } from 'react';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
+import { useState } from 'react';
 
-// const BrandLogo = styled.span`
-// font-family: 'Monoton', cursive;
-// font-weight: 550;
-// `
-// const MainBg = styled.div`
-// height: 65vh;
-// background-image: url('https://www.nanamica.com/upload/main_visual_image/02031108_63dc6c9973d42.jpg');
-// background-size: cover;
-// background-position: center;
-// /* cursor: pointer; */
-// `
-// const StDiv = styled.div`
-// height: 60px;
-// display: flex;
-// justify-content: center;
-// align-items: center;
-// gap: 10px;
-// font-size: .9rem;
-// font-weight: 500;
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import { SlMagnifier } from 'react-icons/sl'
+import { BsBag } from "react-icons/bs";
 
-// `
-// const TextContainer = styled.div`
-// width: 120px;
-// display: flex;
-// text-align: center;
-// justify-content: center;
-// align-items: center;
+const BrandLogo = styled.span`
+font-family: 'Monoton', cursive;
+font-weight: 550;
+`
+const StDiv = styled.div`
+height: 60px;
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 10px;
+font-size: .9rem;
+font-weight: 500;
 
-// height: 60px;
-// /* border-bottom: 1px solid; */
-// /* &:hover {
-// border-bottom: 1px solid
-// } */
-// `
+`
+const TextContainer = styled.div`
+width: 120px;
+display: flex;
+text-align: center;
+justify-content: center;
+align-items: center;
+
+height: 60px;
+/* border-bottom: 1px solid; */
+/* &:hover {
+border-bottom: 1px solid
+} */
+`
 
 function App() {
 
+  const navigate = useNavigate();
+  const [data] = useState([{
+    id: 0,
+    title: "COOLMAX Jersey L/S Tee",
+    content: "nanamica",
+    price: 11000,
+    image: 'https://www.nanamica.com/assets/product_image/sm/6375e434edff5.jpg'
+  },
 
-  // let [data] = useState([{
-  //   id: 0,
-  //   title: "COOLMAX Jersey L/S Tee",
-  //   content: "nanamica",
-  //   price: 11000,
-  //   image: 'https://www.nanamica.com/assets/product_image/sm/6375e434edff5.jpg'
-  // },
+  {
+    id: 1,
+    title: "GORE-TEX Balmacaan Coat",
+    content: "nanamica",
+    price: 121000,
+    image: "https://www.nanamica.com/assets/product_class_image/6375d46e86708.jpg"
+  },
 
-  // {
-  //   id: 1,
-  //   title: "GORE-TEX Balmacaan Coat",
-  //   content: "nanamica",
-  //   price: 121000,
-  //   image: "https://www.nanamica.com/assets/product_class_image/6375d46e86708.jpg"
-  // },
+  {
+    id: 2,
+    title: "Mountain Wind Coach Jacket",
+    content: "THE NORTH FACE PURPLE LABEL",
+    price: 33000,
+    image: "https://www.nanamica.com/assets/product_image/sm/6375bbbaa03ce.jpg"
+  },
+  {
+    id: 3,
+    title: "7G Crew Neck Sweater",
+    content: "nanamica",
+    price: 25300,
+    image: "https://www.nanamica.com/assets/product_class_image/6375d9423bf97.jpg"
+  }]);
+  const [count, setCount] = useState(0);
 
-  // {
-  //   id: 2,
-  //   title: "Mountain Wind Coach Jacket",
-  //   content: "THE NORTH FACE PURPLE LABEL",
-  //   price: 33000,
-  //   image: "https://www.nanamica.com/assets/product_image/sm/6375bbbaa03ce.jpg"
-  // },
-  // {
-  //   id: 3,
-  //   title: "7G Crew Neck Sweater",
-  //   content: "nanamica",
-  //   price: 25300,
-  //   image: "https://www.nanamica.com/assets/product_class_image/6375d9423bf97.jpg"
-  // }]);
-  // let [count, setCount] = useState(0);
+
   return (
-    <>
+    <div>
+      <Navbar className='nav-bar sticky-top' >
+        <Container >
+          <Navbar.Brand style={{
+            cursor: 'pointer'
+          }} className='brand' onClick={() => {
+            navigate('/')
+          }}><BrandLogo>MOOZZANG</BrandLogo> ONLINE STORE</Navbar.Brand>
+          {/* <Nav style={{
+            fontSize: '14px',
+            letterSpacing: '2.5px',
+            fontWeight: '400',
+            cursor: 'pointer'
+          }} onClick={() => { navigate('/Detail') }}> DETAIL</Nav> */}
+          <BsBag style={{ position: 'absolute', right: '90px', cursor: 'pointer' }}></BsBag>
+          <SlMagnifier style={{ position: 'absolute', right: '40px', cursor: 'pointer' }}></SlMagnifier>
+
+        </Container>
+
+
+
+      </Navbar>
+
+      <StDiv>
+        <TextContainer style={count === 1 ? { borderBottom: '1px solid' } : null} onMouseOver={() => { setCount(1) }} onMouseOut={() => {
+          setCount(0);
+        }} >MEN</TextContainer>
+        <TextContainer style={count === 2 ? { borderBottom: '1px solid' } : null} onMouseOver={() => { setCount(2) }} onMouseOut={() => {
+          setCount(0);
+        }}>WOMEN</TextContainer>
+      </StDiv>
+
+
+
+
+
+
+
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/detail' element={<Detail></Detail>}></Route>
+        <Route path='/' element={<Home data={data}></Home>}></Route>
+        <Route path='/detail/:id' element={<Detail data={data} ></Detail>}></Route>
       </Routes>
 
 
-      {/* <div>
-        <Navbar className='nav-bar'>
-          <Container>
-            <Navbar.Brand href="#home" className='brand'><BrandLogo>MOOZZANG</BrandLogo> ONLINE STORE</Navbar.Brand>
-          </Container>
-        </Navbar>
-        <StDiv>
-          <TextContainer style={count === 1 ? { borderBottom: '1px solid' } : null} onMouseOver={() => { setCount(1) }} onMouseOut={() => {
-            setCount(0);
-          }} >MEN</TextContainer>
-          <TextContainer style={count === 2 ? { borderBottom: '1px solid' } : null} onMouseOver={() => { setCount(2) }} onMouseOut={() => {
-            setCount(0);
-          }}>WOMEN</TextContainer>
-        </StDiv>
 
-        <MainBg></MainBg>
-
-        <div className='cardwrap'>
-          {data.map((item) => {
-            return <Card key={item.id} style={{ width: '18rem', border: 'none', fontSize: '.8rem' }}>
-              <Card.Img variant="top" src={item.image} />
-              <Card.Body>
-                <Card.Title style={{ fontSize: '1rem' }}>{item.title}</Card.Title>
-                <Card.Title style={{ fontSize: '1rem' }}>{item.price}YEN</Card.Title>
-                <Card.Text>
-                  {item.content}
-                </Card.Text>
-
-              </Card.Body>
-            </Card>
-          })}
-        </div>
-      </div> */}
-    </>
+    </div>
   );
 }
 
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,65 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components'
+import { BiShoppingBag } from 'react-icons/bi'
 
-function Detail() {
+const OrderButton = styled.button`
+background-color: #1A2848;
+color: white;
+
+width: 300px;
+height: 70px;
+
+border: none;
+
+font-size: .9rem;
+font-weight: 500;
+
+margin-top: 50px;
+
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+
+`
+function Detail(props) {
+    const data = props.data;
+
+    const { id } = useParams();
+    const foundData = data.find((x) => {
+        return x.id === +id
+    })
+    console.log(foundData)
+    // const foudData = data.find((x) => {
+
+    // })
     return (
-        <div>Detail</div>
+        <div className="container" style={{
+            borderBottom: '1px solid lightgray'
+        }}>
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={foundData.image} width="100%" />
+                </div>
+                <div className="col-md-6" style={{
+                    lineHeight: '70px'
+                }}>
+                    <h4 className="pt-5">{foundData.title}</h4>
+                    <p>{foundData.content}</p>
+                    <h4>¥&nbsp;{foundData.price}</h4>
+
+                    <OrderButton>ADD TO CART<BiShoppingBag style={{
+                        position: 'relative',
+                        left: '70px'
+
+                    }}></BiShoppingBag></OrderButton>
+
+
+                </div>
+            </div>
+        </div>
     )
 }
 
